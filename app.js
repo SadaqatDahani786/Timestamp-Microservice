c128abd5905567b760e618150f9494ac6b90ac87
Sadaqat Dahani <sadaqat.dahani2013@gmail.com>
2022-11-12 18:36:06 +0500

    Added view index.html and API routes to get timestamp.

--- app.js
+++ app.js
@@ -12,19 +12,77 @@ const cors = require("cors");
  ** **
  */
 const app = express();
+const PORT = 8080;
 
 /*
  ** **
  ** ** ** MIDDLEWARES
  ** **
  */
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // Some old browsers choke on 204
+app.use(express.static("public")); // Serve static files from public folder
+
+/*
+ ** **
+ ** ** ** ROUTES
+ ** **
+ */
+/**
+ ** =====================================
+ ** Route Index
+ ** =====================================
+ */
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+/**
+ ** =====================================
+ ** Route API start
+ ** =====================================
+ */
+app.get("/api", (req, res) => {
+  //1) Date object
+  const date = new Date();
+
+  //2) Send a response
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+/**
+ ** =====================================
+ ** Route Get Date
+ ** =====================================
+ */
+app.get("/api/:date", (req, res) => {
+  //1) Vars & Init
+  const paramDate = req.params.date;
+  const date = new Date(paramDate);
+
+  //2) Set time in utc utc milliseconds if invalid
+  if (date.toString() === "Invalid Date") date.setTime(paramDate);
+
+  //3) If still Invalid Date, return with error
+  if (!date || date.toString() === "Invalid Date")
+    return res.json({
+      error: "Invalid Date",
+    });
+
+  //4) Send a response
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
 
 /*
  ** **
  ** ** ** HTTP SERVER
  ** **
  */
-app.listen("8080", () => {
-  console.log("Servers is running on port\t\t[8080]");
+app.listen(PORT, () => {
+  console.log(`Servers is running on port:\t[${PORT}]`);
 });
